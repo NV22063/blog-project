5288fef8bdfd528f720e4e2bcbe4f7ab2739713d
NV22063 <155245438+NV22063@users.noreply.github.com>
2024-02-19 09:06:48 +0000

    Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Card from './components/Card';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Card/>
-      </header>
+      <HomePage/>
+
     </div>
   );
 }
