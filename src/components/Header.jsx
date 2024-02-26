7f5827e66584648249b49db3ef111277e35c6b05
NV22063 <155245438+NV22063@users.noreply.github.com>
2024-02-26 09:03:58 +0000

    react icon (do it)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from '../constants'
 
 const NavItemInfo = [
@@ -19,13 +19,21 @@ return (
 };
 
 const Header = () => {
+  const [NavIsVisible, setNavIsVisible] = useState(false);
+
+const NavVisibilityHandler = () => {
+  setNavIsVisible((curState) => {
+    return !curState;
+  });
+};
+
   return (
     <section>
         <header className="container mx-auto px-5 flex justify-between py-4 items-center">
             <div>
-              <img src={images.Logo} alt="logo" />
+              <img className="w-16" src={images.Logo} alt="logo" />
             </div>
-            <div className="flex gap-x-9 items-center">
+            <div className={'${NavIsVisible ? "right-0" : "-right-full"} flex  flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static  gap-x-9 items-center'}>
              <ul className="flex gap-x-2 font-semibold">
                {NavItemInfo.map((item) => (
                 <NavItem Key={item.name} name={item.name}/>
