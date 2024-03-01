1bc54031d4f9de6cb6a05b4660d4568c0b7238df
NV22063 <155245438+NV22063@users.noreply.github.com>
2024-03-01 17:29:20 +0000

    n

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useSyncExternalStore } from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { images } from '../constants'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 
 const NavItemInfo = [
@@ -13,6 +14,14 @@ const NavItemInfo = [
 ]
 
 const NavItem = ({ item }) => {
+const [dropdown, setDropdown] = useState(false);
+
+const toggleDropdownHandler = () => {
+  setDropdown((curState) => {
+    return !curState;
+  });
+};
+
   return (
     <li className="relative group">
       {item.type === "link" ? (
@@ -20,14 +29,14 @@ const NavItem = ({ item }) => {
           <a href="/" className="px-4 py-2">
             {item.name}
           </a>
-          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
+          <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
         </>
       ) : (
-        <>
-        <a href="/" className="px-4 py-2 flex gap-x-1 items-center"><span>{item.name}</span>
+        <div className='flex flex-col items-center'>
+        <button className="px-4 py-2 flex gap-x-1 items-center"><span>{item.name}</span>
         <MdKeyboardArrowDown />
-        </a>
-        <div className='hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max'>
+        </button>
+        <div className='lg:hidden transition-all duration-500 pt-4 lg:absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max'>
           <ul className='flex flex-col shadow-lg rounded-lg overflow-hidden'>
             {item.items.map((page) => (
               <a href="/" className='hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft'
@@ -37,7 +46,7 @@ const NavItem = ({ item }) => {
             ))}
           </ul>
         </div>
-        </>
+        </div>
       )}
     </li>
   );
